b810492519253b984717a0ad599df5eff3f6ca91
cartridge3 <34579329+cartridge3@users.noreply.github.com>
2018-01-11 11:34:01 +0100

    Obfuscating objective names

--- MCFObfuscator/src/me/cartridge3/MCFObfuscator/MCFObfuscator.java
+++ MCFObfuscator/src/me/cartridge3/MCFObfuscator/MCFObfuscator.java
@@ -16,35 +16,58 @@ public class MCFObfuscator {
 	protected static final Logger LOGGER = new Logger();
 	protected static final String PATH = "C:/Users/" + System.getProperty("user.name")
 			+ "/git/MCFObfuscator/MCFObfuscator/src/me/cartridge3/MCFObfuscator/";
+	protected static final String IN = PATH + "test.mcfunction";
+	protected static final String OBJECTIVES = PATH + "obs.txt";
+	protected static final String OUT = PATH + "testOBF.mcfunction";
 
 	// <String,ObfuscatedString>
 	protected static HashMap<String, String> objectivesMap = new HashMap<>();
 
 	public static void main(String[] args) {
 
-		String in = "test.mcfunction";
-		String obs = PATH + "obs.txt";
-		String out = "testOBF.mcfunction";
-
 		LOGGER.log("Starting up...");
 		LOGGER.log("Reading objectives...");
 
-		List<String> objectives = convertObjectives(obs);
+		LOGGER.log("Parsing commands");
+
+		List<String> commands = PARSER.parse(IN);
+
+		LOGGER.log("Obfuscating...");
+
+		List<String> obfCommands = obfuscate(commands);
+
+		for (int i = 0; i < obfCommands.size(); i++) {
+			LOGGER.log(obfCommands.get(i));
+		}
+
+	}
+
+	private static List<String> obfuscate(List<String> commands) {
+
+		int commandCount = commands.size();
+
+		List<String> objectives = convertObjectives(OBJECTIVES);
 
 		LOGGER.log("Obfuscating objective names");
-		
+
 		for (int i = 0; i < objectives.size(); i++) {
 
 			objectivesMap.put(objectives.get(i), generateRandomString(6));
-			
+
 		}
-		
-	
-		
-		
-		
-		
-		
+
+		for (int a = 0; a < objectives.size(); a++) {
+			for (int i = 0; i < commandCount; i++) {
+				if (commands.get(i).contains(objectives.get(a))) {
+
+					commands.set(i,
+							commands.get(i).replaceAll(objectives.get(a), objectivesMap.get(objectives.get(i))));
+
+				}
+			}
+		}
+
+		return commands;
 
 	}
 
@@ -71,28 +94,20 @@ public class MCFObfuscator {
 		return obsList;
 
 	}
-	
-	
+
 	public static String generateRandomString(int length) {
 		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 		StringBuilder sb = new StringBuilder();
 		Random r = new Random();
-		
-		for(int i = 0; i<length; i++) {
+
+		for (int i = 0; i < length; i++) {
 			sb.append(chars.charAt(r.nextInt(chars.length())));
 		}
-		
+
 		LOGGER.log(sb.toString());
-		
+
 		return sb.toString();
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
