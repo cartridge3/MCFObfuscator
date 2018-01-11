6038ee663bad04f4f92a9e64e25e03f3e5e82f69
cartridge3 <34579329+cartridge3@users.noreply.github.com>
2018-01-11 11:33:46 +0100

    Beginning parser

--- MCFObfuscator/src/me/cartridge3/MCFObfuscator/Parser.java
+++ MCFObfuscator/src/me/cartridge3/MCFObfuscator/Parser.java
@@ -1,5 +1,43 @@
 package me.cartridge3.MCFObfuscator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
 
+	
+	public List<String> parse(String filename)
+	{
+		
+		List<String> cmds = new ArrayList<String>();
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+
+			while ((line = br.readLine()) != null) {
+
+				cmds.add(line);
+
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			MCFObfuscator.LOGGER.err("Error whilest reading input file: Cannot find objectives file.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			MCFObfuscator.LOGGER.err("Error whilest reading input file: Failed I/O Operation: " + e.getMessage());
+
+		}
+
+		
+		return cmds;
+		
+		
+	}
+	
+	
 }
